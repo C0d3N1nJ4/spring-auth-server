18c68a60aadeb1b083ddce11864cb01e6a0f90e6
naiomi <Naiomi.Naidoo@icloud.com>
2023-06-19 22:25:08 +0200

    Trying out spring auth server

---
+++ src/test/java/com/spring/auth/server/SpringAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
